5473437ceb3709a5006b2ca09c74b0aac72a7ca8
Morteza Tavanarad <tavanarad@gmail.com>
2017-08-07 12:52:31 +0430

    Changed script file

--- scripts/before-script.sh
+++ scripts/before-script.sh
@@ -2,5 +2,3 @@
 
 git clone "https://$GTOKEN@github.com/morteza-binary/java-api-wrapper-config.git" "binary-api-wrapper/build/resources/test";
 
-cat "binary-api-wrapper/build/resources/test/TestEnvironment.properties"
-
