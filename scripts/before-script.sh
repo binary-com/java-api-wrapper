fd9572a68e623d4814f1f5b2678ec7112ec1b8cc
Morteza Tavanarad <tavanarad@gmail.com>
2017-08-07 08:12:59 +0430

    change before-script.sh

--- scripts/before-script.sh
+++ scripts/before-script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-git clone "https://$GTOKEN@github.com/morteza-binary/java-api-wrapper-config" "binary-api-wrapper/build/resources/test/resources"
+git clone "https://$GTOKEN@github.com/morteza-binary/java-api-wrapper-config" "~/morteza-binary/java-api-wrapper/binary-api-wrapper/build/resources/test/resources";
+
+cat $(ls "~/morteza-binary/java-api-wrapper/binary-api-wrapper/build/resources/test/resources");
